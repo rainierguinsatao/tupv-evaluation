a10302aa57c5355d46eaf4769499f6705dc09da0
ameqtpie <rainierguinsatao09@gmail.com>
2023-11-09 21:40:27 +0800

    basta amo na ah

--- _admin/admin.php
+++ _admin/admin.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../dist/output.css" rel="stylesheet">
-    <title>Document</title>
+    <title>ADMIN | Evaluation</title>
 </head>
 <body>
 <form action="../php/logout_admin.php">
